1aac2b27a40a424118885c63061a013f8cbdbba4
ManupaDev <manupadevsuite@gmail.com>
2023-01-20 17:51:57 +0530

    setup showcase section

---
+++ app/components/showcase.tsx
@@ -0,0 +1,12 @@
+function Showcase() {
+  return (
+    <div className="lg:px-32 lg:py-32">
+      <div>
+        <h1 className="text-6xl font-sans">New to the scene,</h1>
+        <h1 className="text-6xl font-sans mt-2">but making a big impact</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Showcase;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./components/hero";
+import Showcase from "./components/showcase";
 
 function Home() {
   return (
     <div className="border border-black">
       <Hero />
+      <Showcase/>
     </div>
   );
 }
