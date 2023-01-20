bf596d21ce490d207c7b7fdec47743d0343431e9
ManupaDev <manupadevsuite@gmail.com>
2023-01-20 17:42:09 +0530

    edited hero section

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -11,9 +11,9 @@ function Hero() {
         <h1 className="text-5xl">tools we use</h1>
         <div className="flex mt-14 border-black justify-between flex-wrap">
           <Image src={"/assets/js-logo.svg"} width={48} height={48} alt={"Javascript Logo"} />
-          <Image src={"/assets/figma-logo.svg"} width={160} height={48} alt={"Javascript Logo"} />
-          <Image src={"/assets/flutter-logo.svg"} width={160} height={48} alt={"Javascript Logo"} />
-          <Image src={"/assets/react-logo.svg"} width={160} height={48} alt={"Javascript Logo"} />
+          <Image src={"/assets/figma-logo.svg"} width={160} height={48} alt={"Figma Logo"} />
+          <Image src={"/assets/flutter-logo.svg"} width={160} height={48} alt={"Flutter Logo"} />
+          <Image src={"/assets/react-logo.svg"} width={160} height={48} alt={"React Logo"} />
           <Image src={"/assets/swift-logo.svg"} width={160} height={48} alt={"Javascript Logo"} />
           <Image src={"/assets/tailwindcss-logo.svg"} width={300} height={48} alt={"Javascript Logo"} />
         </div>
