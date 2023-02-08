38d1af7380a838d054b65cf6965335b9bbe6020b
ManupaDev <manupadevsuite@gmail.com>
2023-02-08 15:13:55 +0530

    feat:showcase

--- app/components/showcase.tsx
+++ app/components/showcase.tsx
@@ -1,10 +1,10 @@
 function Showcase() {
   return (
-    <div className="py-4 border border-black">
+    <div className="py-4 border border-black min-h-screen">
       <h1 className="text-3xl">
         New to the scene, but already making a <span>big impact</span>
       </h1>
-      <div className="flex flex-col min-h-screen"></div>
+      <div className="flex flex-col "></div>
     </div>
   );
 }
