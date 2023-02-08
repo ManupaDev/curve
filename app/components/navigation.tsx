4d9b44ac5611787ad3b0421258297adc1fd5cb6f
ManupaDev <manupadevsuite@gmail.com>
2023-02-08 14:54:16 +0530

    chore:added bounds to navigation

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -2,7 +2,7 @@ import { Bars2Icon } from "@heroicons/react/20/solid";
 
 function Navigation() {
     return ( 
-    <div className="px-8 flex justify-between items-center py-6">
+    <div className="px-8 flex justify-between items-center py-4 border border-black ">
         <div></div>
         <Bars2Icon width={20} height={20}/>
     </div> );
