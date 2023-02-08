e39b266df8673fc0f6d96649c349e6112ca4f67d
ManupaDev <manupadevsuite@gmail.com>
2023-02-08 14:57:34 +0530

    feat:showcase

---
+++ app/components/showcase.tsx
@@ -0,0 +1,12 @@
+function Showcase() {
+  return (
+    <div className="py-4 border border-black">
+      <h1 className="text-3xl">
+        New to the scene, but already making a <span>big impact</span>
+      </h1>
+      <div className="flex flex-col min-h-screen"></div>
+    </div>
+  );
+}
+
+export default Showcase;
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,14 +13,7 @@ module.exports = {
         ['sans']:['product-sans',...defaultTheme.fontFamily.sans]
       },
       colors:{
-        'curve-green':'#309C8A',
-        'curve-green-light':'#4BC9B5',
-        'curve-grey':'#9D9D9D',
-        'curve-grey-darker':'#3B3B3B',
-        'curve-grey-lighter':'#6A6A6A',
-        'curve-grey-dark':'#5F5E5E',
-        'curve-grey-light':'#9D9D9D',
-        'curve-blue':'#1C5294'
+        
       }
     },
   },
