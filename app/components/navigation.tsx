7fbd4dcdbff69a2f79320615fd4afac1e907aa31
ManupaDev <manupadevsuite@gmail.com>
2023-02-08 13:47:44 +0530

    feat:base navigation

---
+++ app/components/navigation.tsx
@@ -0,0 +1,11 @@
+import { Bars2Icon } from "@heroicons/react/20/solid";
+
+function Navigation() {
+    return ( 
+    <div className="px-8 flex justify-between items-center py-6">
+        <div></div>
+        <Bars2Icon width={20} height={20}/>
+    </div> );
+}
+
+export default Navigation;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import "./globals.css";
+import Navigation from "./components/navigation";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head />
       <body>
+        <Navigation />
         {children}
       </body>
     </html>
