09f47f01ae1b150e1e3044f905cd2519ccbb844e
ManupaDev <manupadevsuite@gmail.com>
2023-02-08 08:19:35 +0530

    overhaul:archive previous dev build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,10 @@
 import "./globals.css";
-import Navigation from "./components/navigation";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head />
       <body>
-        <Navigation/>
         {children}
       </body>
     </html>
