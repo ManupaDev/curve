8b7312444275f68926feeac4a8060ad3294054c6
ManupaDev <manupadevsuite@gmail.com>
2023-02-08 15:14:10 +0530

    feat:home services

---
+++ app/components/services.tsx
@@ -0,0 +1,11 @@
+function Services() {
+  return (
+    <div className="min-h-screen py-8 border border-black">
+      <h1 className="text-3xl">
+        Our services / <span>what we do</span>{" "}
+      </h1>
+    </div>
+  );
+}
+
+export default Services;
