90150f0c64f9581e745a3b7deb699c16fd2b7687
ManupaDev <manupadevsuite@gmail.com>
2023-02-10 00:30:20 +0530

    feat: optimized font in home/services

--- app/components/services.tsx
+++ app/components/services.tsx
@@ -1,9 +1,14 @@
+/* eslint-disable react/no-unescaped-entities */
 function Services() {
   return (
-    <div className="min-h-screen py-8 border border-black">
+    <div className="min-h-screen border border-black py-8">
       <h1 className="text-3xl">
-        Our services / <span>what we do</span>{" "}
+        Our services / <span>what we do</span>
       </h1>
+      <p className="text-base mt-4">
+        From crafting the perfect name, designing a killer website to making sure it's easy to use, we've got it all. We
+        also focus on consistency across all mediums and touchpoints, so you can be sure your brand is always on point.
+      </p>
     </div>
   );
 }
