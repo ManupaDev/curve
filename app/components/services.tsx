bffa7aa57d4f86fcc949d3f1f0c8eff9d48b139f
ManupaDev <manupadevsuite@gmail.com>
2023-02-10 09:46:50 +0530

    feat: home services on xl

--- app/components/services.tsx
+++ app/components/services.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/no-unescaped-entities */
 function Services() {
   return (
-    <div className="min-h-screen border border-black py-8">
-      <h1 className="text-3xl">
+    <div className="min-h-screen border border-black py-8 xl:py-32">
+      <h1 className="text-3xl xl:text-6xl">
         Our services / <span>what we do</span>
       </h1>
-      <p className="text-base mt-4">
+      <p className="text-base xl:text-3xl mt-4 xl:mt-32">
         From crafting the perfect name, designing a killer website to making sure it's easy to use, we've got it all. We
         also focus on consistency across all mediums and touchpoints, so you can be sure your brand is always on point.
       </p>
