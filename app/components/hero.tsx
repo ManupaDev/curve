a9e1f8252e3e4d864ccb2eea63d0a29320af01ca
ManupaDev <manupadevsuite@gmail.com>
2023-02-25 21:38:46 +0530

    fix:home hero tools icon grid

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -8,17 +8,17 @@ import TailwindLogo from "./../../public/assets/tailwindcss-logo.svg";
 
 function Hero() {
   return (
-    <div className="flex min-h-screen flex-col xl:gap-y-16 justify-between border border-black py-16 xl:py-32">
+    <div className="flex min-h-screen flex-col justify-between border border-black py-16 xl:gap-y-16 xl:py-32">
       <div>
         <h1 className="text-5xl xl:text-6xl">
           Introducing the newest player in town, delivering top-notch quality <span>in all that we do.</span>
         </h1>
-        <h3 className="text-2xl mt-2 xl:text-3xl xl:mt-4 underline">More about our services</h3>
+        <h3 className="mt-2 text-2xl underline xl:mt-4 xl:text-3xl">More about our services</h3>
       </div>
 
       <div>
-        <h1 className="text-base  xl:text-4xl">tools we use</h1>
-        <div className="mt-4 xl:mt-8 flex flex-row flex-wrap justify-center gap-4 sm:gap-8 xl:gap-x-20 xl:gap-y-4">
+        <h1 className="mt-16 text-base xl:text-4xl">tools we use</h1>
+        <div className="mt-2 xl:mt-6 grid grid-cols-3 place-items-center  gap-x-4 gap-y-8 px-2">
           <Image width={20} className="sm:scale-125 xl:scale-175" src={JSLogo} alt="js-logo" />
           <Image width={72} className="sm:scale-125 xl:scale-175" src={FigmaLogo} alt="figma-logo" />
           <Image width={72} className="sm:scale-125 xl:scale-175" src={FlutterLogo} alt="flutter-logo" />
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -5,29 +5,14 @@ function Services() {
   return (
     <div className="font-sans">
       <Hero />
-      <div className="px-12 py-16 border border-black">
+
+      <div className="border border-black px-12 py-16">
         <h1 className="text-2xl underline">Branding</h1>
-        <h1 className="text-3xl mt-8">We make brands, or give the old ones a facelift</h1>
-        <p className="text-base mt-8">
+        <h1 className="mt-8 text-3xl">We make brands, or give the old ones a facelift</h1>
+        <p className="mt-8 text-base">
           We're in the business of building awesome digital products for tech companies and beyond, from creating
           <span>new brands</span> to <span>designing a killer website</span> and <span>user experience.</span>
         </p>
-      </div>
-      <div className="px-1 flex"> 
-        <div className="h-24 border-l-4 border-dashed border-black w-24"></div>
-        <div>
-          <h1 className="text-lg">Brand Name</h1>
-          <h2 className="text-sm">Brand Name Consultancy</h2>
-          <h2 className="text-sm">Brand Name Creation</h2>
-          <h2 className="text-sm">Brand Name Availabbility</h2>
-        </div>
-        <div className="h-24 border-l-4 border-dashed border-black w-24"></div>
-        <div>
-          <h1 className="text-lg">Brand Book</h1>
-          <h2 className="text-sm">Brand Name Consultancy</h2>
-          <h2 className="text-sm">Brand Name Creation</h2>
-          <h2 className="text-sm">Brand Name Availabbility</h2>
-        </div>
       </div>
     </div>
   );
