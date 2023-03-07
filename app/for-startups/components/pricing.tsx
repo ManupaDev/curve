bea704f4107a5c2fee8a91d04e9fcdee5086cf3b
ManupaDev <manupadevsuite@gmail.com>
2023-03-07 10:06:56 +0530

    feat:for startup pricing section

---
+++ app/for-startups/components/pricing.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/no-unescaped-entities */
+import StartupPricingCards from "@/app/pricing/components/startup-pricing-cards";
+
+function Pricing() {
+  return (
+    <div className="py-16 xl:py-32">
+      <div className="px-12 xl:px-72">
+        <h1 className="text-center text-4xl xl:text-6xl">Packages and Pricing</h1>
+        <p className="mt-8 text-center text-base xl:mt-16 xl:text-3xl">
+          From sparking of branding to a full blast of website development and UI/UX design, we've got you covered. Our
+          pricing is transparent and simple to understand, making the selection process a breeze.
+        </p>
+      </div>
+      <StartupPricingCards />
+      <h1 className="mt-32  px-12  text-lg xl:px-72 xl:text-4xl">Learn more about Start-up Packages and Pricing+</h1>
+    </div>
+  );
+}
+
+export default Pricing;
